3a66271c4dc813f34999ef15c2c052833451cc67
Martijn Becker <martijn@becker.wtf>
2017-04-25 00:50:58 +0200

    f*540923984jpdfgjklnpsdfkl;sdfknlsdf

--- load.js
+++ load.js
@@ -2,7 +2,7 @@
 
 const select = selector => document.querySelector(selector)
 
-const selectALL = selector => document.querySelectorAll(selector)
+const selectAll = selector => document.querySelectorAll(selector)
 
 const loadScript = _ => {
 	let script = document.createElement("script")
@@ -22,20 +22,42 @@ window.onresize = _ => {
 
 window.onload = window.onresize
 
+// look I'll fix this later lol.
 function moveCursor() {
-	const options = selectAll('.options')
-	
+	const options = select('.options')
+	options.innerHTML = options.innerHTML
+	.replace(/<span class="cursor.*?<\/span>/, "<span")
 }
+
+
+
+// maybe replace this with regex?
+// function moveCursor() {
+// 	const options = selectAll('.option')
+// 	const cursor = select('cursor')
+	
+// 	options.forEach(option => {
+// 		if (option === cursor){
+// 				select('cursor').setAttribute('class', 'option')
+// 				option.setAttribute('class', 'option cursor')
+// 				console.log(option)
+// 		}
+// 	})
+// }
+
+// function moveCursorLeft() {
+	// moveCursor
+
 
 const keys = []
 keys[32] = loadScript
-keys[37] = moveCursor 
-keys[38] = moveCursor
-keys[39] = moveCursor
-keys[40] = moveCursor
+// keys[37] = moveCursor
+// keys[38] = moveCursor
+// keys[39] = moveCursor
+// keys[40] = moveCursor
 
 // call the function corresponding to the key.
-window.onkeydown = e => keys[e.keyCode]() && e
+window.onkeydown = e => keys[e.keyCode]()
 window.onclick = loadScript
 
 //document.querySelectorAll('.option').forEach(el => el.onclick = ev => el.setAttribute('class','active'))
