1b01003f65cf70f135daa16ee9adaa63498f1927
Martijn Becker <martijn@becker.wtf>
2017-05-07 03:33:22 +0200

    Added default test

---
+++ test/test.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+const chai = require('chai')
+const assert = chai.assert
+//const asap = require('../')
+
+// todo: add real unit tests
+describe('true', function () {
+    it('equals true', function () {
+        assert.equal(true, true)
+    })
+})
